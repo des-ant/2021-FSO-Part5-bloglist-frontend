3dc8f67eabfa54d29c2db52f3abbe44d1319a6a1
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-26 17:31:53 +1100

    Refactored LoginForm into separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
+import LoginForm from './components/LoginForm';
 import blogService from './services/blogs';
 import loginService from './services/login';
 
@@ -30,35 +31,21 @@ const App = () => {
     }
   };
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  );
+  const handleUsernameChange = ({ target }) => setUsername(target.value);
+
+  const handlePasswordChange = ({ target }) => setPassword(target.value);
 
   if (user === null) {
     return (
       <div>
         <h2> Log in to application</h2>
-        {loginForm()}
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePasswordChange}
+        />
       </div>
     );
   }
