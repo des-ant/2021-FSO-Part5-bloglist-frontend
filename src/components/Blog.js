52917fbbe21c4ba6913110ab701ce3a2513c0294
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-28 15:21:13 +1100

    Added a button to each blog which controls whether all of the details about the blog are shown or not

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+  
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -12,8 +21,10 @@ const Blog = ({ blog }) => {
     <div style={blogStyle}>
       <div>
         {blog.title} {blog.author}
+        <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+        <button style={showWhenVisible} onClick={toggleVisibility}>hide</button>
       </div>
-      <div>
+      <div style={showWhenVisible}>
         {blog.url}
         <br/>
         likes {blog.likes} <button>like</button>
