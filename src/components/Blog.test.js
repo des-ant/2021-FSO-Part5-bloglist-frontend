a55142500d2fa9cfb21dc04ed4d4f54d35f94e26
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-30 23:34:31 +1100

    Added a test which checks that the blog's url and number of likes are shown when the button controlling the shown details has been clicked -ex5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,55 +1,80 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { prettyDOM } from '@testing-library/dom';
 import Blog from './Blog';
 
-test('renders title and author but not url or number of likes by default', () => {
-  const blog = {
-    title: 'Blog title should be rendered',
-    author: 'Blog author should also be rendered',
-    url: 'Blog url should not be rendered by default'
-  };
+describe('<Blog />', () => {
+  let component;
 
-  const mockHandler = jest.fn();
-  const username = 'mockUser';
+  beforeEach(() => {
+    const blog = {
+      title: 'Blog title should be rendered',
+      author: 'Blog author should also be rendered',
+      url: 'Blog url should not be rendered by default'
+    };
 
-  const component = render(
-    <Blog
-      blog={blog}
-      increaseLikes={mockHandler}
-      deleteBlog={mockHandler}
-      username={username}
-    />
-  );
+    const mockHandler = jest.fn();
+    const username = 'mockUser';
 
-  expect(component.container).toHaveTextContent(
-    'Blog title should be rendered'
-  );
+    component = render(
+      <Blog
+        blog={blog}
+        increaseLikes={mockHandler}
+        deleteBlog={mockHandler}
+        username={username}
+      />
+    );
+  });
 
-  expect(component.container).toHaveTextContent(
-    'Blog author should also be rendered'
-  );
+  test('renders title and author but not url or number of likes by default', () => {
+    expect(component.container).toHaveTextContent(
+      'Blog title should be rendered'
+    );
 
-  const blogTitleAuthor = component.getByText(
-    'Blog title should be rendered Blog author should also be rendered'
-  );
-  expect(blogTitleAuthor).toBeVisible();
+    expect(component.container).toHaveTextContent(
+      'Blog author should also be rendered'
+    );
 
-  const blogUrl = component.getByText(
-    'Blog url should not be rendered by default'
-  );
-  expect(blogUrl).not.toBeVisible();
-  const blogUrlParent = blogUrl.parentElement;
-  expect(blogUrlParent).toHaveStyle('display: none');
+    const blogTitleAuthor = component.getByText(
+      'Blog title should be rendered Blog author should also be rendered'
+    );
+    expect(blogTitleAuthor).toBeVisible();
 
-  const blogLikes = component.getByText(
-    'likes'
-  );
-  expect(blogLikes).not.toBeVisible();
-  const blogLikesParent = blogLikes.parentElement;
-  expect(blogLikesParent).toHaveStyle('display: none');
+    const blogUrl = component.getByText(
+      'Blog url should not be rendered by default'
+    );
+    expect(blogUrl).not.toBeVisible();
+    const blogUrlParent = blogUrl.parentElement;
+    expect(blogUrlParent).toHaveStyle('display: none');
 
-  console.log(prettyDOM(blogLikesParent));
+    const blogLikes = component.getByText(
+      'likes'
+    );
+    expect(blogLikes).not.toBeVisible();
+    const blogLikesParent = blogLikes.parentElement;
+    expect(blogLikesParent).toHaveStyle('display: none');
+  });
 
+  test('blogs url and number of likes are shown when the button controlling the shown details has been clicked', () => {
+    const viewButton = component.getByText('view');
+    fireEvent.click(viewButton);
+
+    console.log(prettyDOM(component.container));
+
+    const blogUrl = component.getByText(
+      'Blog url should not be rendered by default'
+    );
+    expect(blogUrl).toBeVisible();
+    const blogUrlParent = blogUrl.parentElement;
+    expect(blogUrlParent).not.toHaveStyle('display: none');
+
+    const blogLikes = component.getByText(
+      'likes'
+    );
+    expect(blogLikes).toBeVisible();
+    const blogLikesParent = blogLikes.parentElement;
+    expect(blogLikesParent).not.toHaveStyle('display: none');
+  });
 });
+
