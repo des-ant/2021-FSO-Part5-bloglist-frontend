ec09c959a9e350e42253afd658fecaaa02b88f37
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-02 11:08:41 +1100

    Added Cypress test to ensure that app displays login form by default -ex5.17

---
+++ cypress/integration/blog_app.spec.js
@@ -0,0 +1,15 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function() {
+    cy.get('#username');
+    cy.contains('username');
+    cy.get('#password');
+    cy.contains('password');
+    cy.get('#login-button');
+    cy.contains('login');
+  });
+});
