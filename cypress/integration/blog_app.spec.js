98ab82cf49ccf07889f45d190838e0c1138cd9ce
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-02 11:40:47 +1100

    Added Cypress tests for logging in -ex5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      name: 'Superuser',
+      username: 'root',
+      password: 'salainen'
+    };
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
@@ -11,5 +17,27 @@ describe('Blog app', function() {
     cy.contains('password');
     cy.get('#login-button');
     cy.contains('login');
+  });
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('root');
+      cy.get('#password').type('salainen');
+      cy.get('#login-button').click();
+
+      cy.contains('Superuser logged in');
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('root');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+
+      cy.get('.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)');
+
+      cy.get('html').should('not.contain', 'Superuser logged in');
+    });
   });
 });
